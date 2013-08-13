43d7aee4407c5e3f4636c147e29f6e2033846b21
wuwb <wuwb@ucweb.com>
2013-08-13 14:06:13 +0800

    提交概念思维导图

---
+++ doc/游戏引擎概念.mm
@@ -0,0 +1,267 @@
+
+<map version="0.9.0">
+    <node ID="FFCC7E99-BAF1-4925-A7FE-670B339DCAB8" TEXT="游戏" FOLDED="false" COLOR="#000000">
+        <font NAME="STHeitiSC-Light" SIZE="13"></font>
+        <edge COLOR="#7F7F7F"></edge>
+        <node ID="C27BF746-AA2F-4D91-AC33-F5F778D7962C" TEXT="社交系统" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#E5CB65"></edge>
+            <node ID="EA4EABDC-6639-458F-A734-D79038C7CD1E" TEXT="排名" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#E5CB65"></edge>
+            </node>
+            <node ID="040574F1-0BB5-462B-9450-1406FA9E3B0E" TEXT="互助" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#E5CB65"></edge>
+                <node ID="829517C2-AB83-46B3-94A7-316E3A9AE4DF" TEXT="送礼" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#E5CB65"></edge>
+                </node>
+                <node ID="9BDE8D9A-A997-4FAB-989B-529651953A55" TEXT="召唤" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#E5CB65"></edge>
+                    <node ID="7338049C-F586-4901-8F3A-570FDBBF96B0" TEXT="无偿召唤" FOLDED="false" COLOR="#000000">
+                        <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                        <edge COLOR="#E5CB65"></edge>
+                    </node>
+                    <node ID="4A4960F7-5CB3-4E00-85A4-7801F4BC2905" TEXT="有偿召唤" FOLDED="false" COLOR="#000000">
+                        <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                        <edge COLOR="#E5CB65"></edge>
+                    </node>
+                    <node ID="0A0FDC13-BC25-4275-9ECD-C3D66B66502A" TEXT="可离线召唤" FOLDED="false" COLOR="#000000">
+                        <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                        <edge COLOR="#E5CB65"></edge>
+                    </node>
+                </node>
+                <node ID="35E61E64-A71E-4EC3-A685-4DE4F3D0F1CA" TEXT="组队" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#E5CB65"></edge>
+                    <node ID="937A9582-4ED2-456A-9E0C-1BD5FEB5FBA6" TEXT="要求同时在线" FOLDED="false" COLOR="#000000">
+                        <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                        <edge COLOR="#E5CB65"></edge>
+                    </node>
+                    <node ID="06D34B49-7664-419F-909D-00E94D170354" TEXT="可随机" FOLDED="false" COLOR="#000000">
+                        <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                        <edge COLOR="#E5CB65"></edge>
+                    </node>
+                    <node ID="89CEE25E-192F-49A4-995A-BB5F7264B9F6" TEXT="可固定" FOLDED="false" COLOR="#000000">
+                        <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                        <edge COLOR="#E5CB65"></edge>
+                        <node ID="2E7F6D4E-D057-4AFD-ACA3-3335B3F43735" TEXT="工会" FOLDED="false" COLOR="#000000">
+                            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                            <edge COLOR="#E5CB65"></edge>
+                        </node>
+                    </node>
+                </node>
+            </node>
+        </node>
+        <node ID="AD246F91-8CD3-4F6B-BC8D-23BF7931696C" TEXT="教程系统" POSITION="right" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#CB9933"></edge>
+            <node ID="8058C8EA-79E4-4002-B5D0-9C6F42672C2D" TEXT="功能tips" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#CB9933"></edge>
+            </node>
+            <node ID="415793F6-5E9C-4812-AF1B-74C4ECB9F6F1" TEXT="新手引导" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#CB9933"></edge>
+            </node>
+        </node>
+        <node ID="E831AEB5-D1CE-4ECA-AC00-C8CA8BFB1958" TEXT="怪物系统" POSITION="right" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#4CE5B2"></edge>
+            <node ID="E9595A40-F284-4D16-81D6-9D7B8A834DCB" TEXT="刷新概率" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#4CE5B2"></edge>
+            </node>
+            <node ID="53041053-B7B9-49EA-B251-0D369963EE5A" TEXT="等级" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#4CE5B2"></edge>
+            </node>
+            <node ID="70CE21C5-3363-4A78-9FA4-D2438FBF6B0C" TEXT="通常包含一个掉落" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#4CE5B2"></edge>
+            </node>
+        </node>
+        <node ID="BAAA2F8D-03C8-4211-B891-9179CAF3C6AA" TEXT="掉落系统" POSITION="right" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#FFCC66"></edge>
+            <node ID="686667B2-47D0-47D5-84F9-54134F36FB56" TEXT="掉落概率" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#FFCC66"></edge>
+                <node ID="DC429A0F-6CE2-42B0-971C-ED77C9C4B476" TEXT="必掉" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#FFCC66"></edge>
+                </node>
+                <node ID="5E75BD16-D0E7-4DD5-9C00-CE1B9F42A683" TEXT="可能掉" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#FFCC66"></edge>
+                </node>
+            </node>
+            <node ID="E41DBE96-A98F-4B9B-B9AA-2D741599D773" TEXT="宝物池" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#FFCC66"></edge>
+            </node>
+        </node>
+        <node ID="11D39C69-7C19-46FB-924E-7A761AF00B5D" TEXT="道具系统" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#B2E54C"></edge>
+            <node ID="D7E21078-B31D-43BB-B57A-11AD24AD0CDD" TEXT="消耗性道具" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#B2E54C"></edge>
+                <node ID="D828ECB5-1C55-46AA-8463-A0CE3EE076C7" TEXT="任务物品" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#B2E54C"></edge>
+                </node>
+                <node ID="C41108D5-8520-4625-B4CD-4FDEECDFD33E" TEXT="可使用物品" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#B2E54C"></edge>
+                </node>
+                <node ID="1DF954A6-614E-40C7-99B0-2E1C6113CF93" TEXT="金币or点券" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#B2E54C"></edge>
+                </node>
+            </node>
+            <node ID="F098D149-16C3-4CD3-B3AF-6C3D17FA4360" TEXT="永久性道具" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#B2E54C"></edge>
+                <node ID="9DDB7C8D-8758-4317-BC41-FF49BF1D2498" TEXT="装备" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#B2E54C"></edge>
+                    <node ID="50CEA952-92D1-4E2D-818C-98E9358F3FF9" TEXT="考虑包含耐久度设定" FOLDED="false" COLOR="#000000">
+                        <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                        <edge COLOR="#B2E54C"></edge>
+                    </node>
+                </node>
+            </node>
+        </node>
+        <node ID="D32E9214-08A1-4B20-B698-3606B6437EE0" TEXT="交易系统" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#66CCFF"></edge>
+        </node>
+        <node ID="E5B9DBEB-A12E-4658-9D18-E95654DE8163" TEXT="计时系统" POSITION="right" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#CCFF66"></edge>
+            <node ID="D4491BC4-51BE-4E60-8FC2-0A0AC96E03BB" TEXT="作用范围" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#CCFF66"></edge>
+                <node ID="C2841AE6-DF5B-4B1B-8876-7E539F6B1410" TEXT="全局计时" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#CCFF66"></edge>
+                </node>
+                <node ID="15D9CDC3-041B-4DBD-8953-07C4766E04B8" TEXT="个人计时" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#CCFF66"></edge>
+                </node>
+            </node>
+            <node ID="9C350E03-FC51-49B2-A90F-6B917B08476B" TEXT="类型" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#CCFF66"></edge>
+                <node ID="2B3CCDC3-8B70-4E0C-97DD-072A17FEE59D" TEXT="buff" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#CCFF66"></edge>
+                </node>
+                <node ID="E608DC3E-9973-45E1-807C-B77343E7F244" TEXT="技能CD" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#CCFF66"></edge>
+                </node>
+                <node ID="975EB3D8-85CC-48CD-A0D4-44E25BA78234" TEXT="任务" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#CCFF66"></edge>
+                </node>
+            </node>
+        </node>
+        <node ID="4FF1701D-3D7A-471D-93B1-855FC73C6D8F" TEXT="任务系统" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#66FFCC"></edge>
+            <node ID="E88777CA-1FB9-43BD-AF63-02A6AA5A488D" TEXT="触发条件" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#66FFCC"></edge>
+            </node>
+            <node ID="69E7327C-882B-4896-9154-8DD8BA05AE29" TEXT="任务奖励" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#66FFCC"></edge>
+                <node ID="BC18F6C8-25C2-4462-BC81-9E3D60FA4F62" TEXT="包含一个掉落" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#66FFCC"></edge>
+                </node>
+            </node>
+            <node ID="D4802C5B-DBF7-4582-8F51-5BE29A33A2CD" TEXT="类型" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#66FFCC"></edge>
+                <node ID="FC895795-9D1E-4B12-9BFD-B82B21EAE7FF" TEXT="主线" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#66FFCC"></edge>
+                </node>
+                <node ID="E1EC296A-8D5B-4199-A359-97BBA5A5E95E" TEXT="分支" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#66FFCC"></edge>
+                </node>
+            </node>
+        </node>
+        <node ID="4B31AF38-063A-49E9-83E0-C259BB73E9FC" TEXT="背包系统" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#E5B24C"></edge>
+            <node ID="B3173DBF-C2DA-44C6-AE1C-DD52F1B36026" TEXT="容量" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#E5B24C"></edge>
+            </node>
+            <node ID="7350604E-A04D-4CB3-B0FC-FC5469A9639E" TEXT="使用原子性" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#E5B24C"></edge>
+            </node>
+        </node>
+        <node ID="25E1238C-15B4-4AB9-9D34-F111E29A169D" TEXT="成长体系" POSITION="right" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#FFE57F"></edge>
+            <node ID="BD44F90F-3327-4A90-9CBA-0FE857418ACF" TEXT="等级" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#FFE57F"></edge>
+            </node>
+            <node ID="631664D9-57CF-446B-8CD5-6F0A98B257C8" TEXT="经验" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#FFE57F"></edge>
+            </node>
+            <node ID="4F95C8CA-041D-4F1E-A03A-C55A6233510B" TEXT="技能" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#FFE57F"></edge>
+            </node>
+            <node ID="8629D81E-A46C-46D9-A6FF-5C3525E0947D" TEXT="天赋" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#FFE57F"></edge>
+                <node ID="CD4C549E-B27F-4DAB-AA6C-8D2C2DDEBC62" TEXT="固有天赋" FOLDED="false" COLOR="#000000">
+                    <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                    <edge COLOR="#FFE57F"></edge>
+                </node>
+            </node>
+            <node ID="8D6823CA-7480-4A4C-8283-5DDD02F9CE9E" TEXT="成就" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#FFE57F"></edge>
+            </node>
+        </node>
+        <node ID="3628985C-29A8-47D8-8F00-70AAE06ADAC4" TEXT="战斗系统" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#E5B24C"></edge>
+        </node>
+        <node ID="C0C8C5AF-7306-4F3B-AB52-C0BCBEFB69CC" TEXT="吸引人的要素" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#B24CE5"></edge>
+            <node ID="EE371616-DA02-4AD1-B063-EA4E27D063E9" TEXT="满足虚荣型" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#B24CE5"></edge>
+            </node>
+            <node ID="C8CD726D-6D00-47CE-B7D4-90609A04DA81" TEXT="能够装B" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#B24CE5"></edge>
+            </node>
+            <node ID="5DA173C1-0D1B-4FF7-BFC1-40BF752DAD8C" TEXT="容易上瘾" FOLDED="false" COLOR="#000000">
+                <font NAME="STHeitiSC-Light" SIZE="12"></font>
+                <edge COLOR="#B24CE5"></edge>
+            </node>
+        </node>
+        <node ID="944BC0A0-7445-4203-B87E-2D5AADD6CD3D" TEXT="AI系统" POSITION="left" FOLDED="false" COLOR="#000000">
+            <font NAME="STHeitiSC-Light" SIZE="12"></font>
+            <edge COLOR="#B24CE5"></edge>
+        </node>
+    </node>
+</map>
